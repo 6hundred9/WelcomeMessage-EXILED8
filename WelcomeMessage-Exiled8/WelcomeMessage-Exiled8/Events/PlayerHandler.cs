519b6b6c70e4557bf3c5b83725ef5e496c0c6930
6hundred9 <96420090+RobloxianCoder@users.noreply.github.com>
2024-03-22 23:08:34 +0300

    Add files via upload

---
+++ WelcomeMessage-Exiled8/WelcomeMessage-Exiled8/Events/PlayerHandler.cs
@@ -0,0 +1,19 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using Exiled.Events.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeMessage_Exiled8.Events
+{
+    internal class PlayerHandler : Plugin<Config>
+    {
+        public static void OnVerified(VerifiedEventArgs ev)
+        {
+            ev.Player.Broadcast(5, "Welkum to hell " + ev.Player.DisplayNickname);
+        }
+    }
+}
---
+++ WelcomeMessage-Exiled8/WelcomeMessage-Exiled8/WelcomeMessage.cs
@@ -0,0 +1,21 @@
+﻿using Exiled.API.Features;
+using Player = Exiled.Events.Handlers.Player;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PlayerList;
+
+namespace WelcomeMessage_Exiled8
+{
+    public class WelcomeMessage : Plugin<Config>
+    {
+        public override void OnEnabled()
+        {
+            Player.Verified += Player.OnVerified;
+            base.OnEnabled();
+        }
+    }
+}
